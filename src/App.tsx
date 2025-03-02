e9f6f2e86e55a03ad8cb604b143bd86b5b154788
Andrii Artemenko <andrii.artemenko@maersk.com>
2025-03-02 01:17:03 +0100

    Add numbers

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,8 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+        <p>123</p>
+        <p>456</p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
