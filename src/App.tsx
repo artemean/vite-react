84e7a5375fac1f4025f29ac3a1a6b42e40ea9525
Andrii Artemenko <andrii.artemenko@maersk.com>
2025-03-02 01:43:17 +0100

    Add test key

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const testKey = import.meta.env.VITE_TEST_KEY;
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+        <h2>Test key is {testKey}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
